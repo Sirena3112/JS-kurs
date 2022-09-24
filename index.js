d7db08657d24cf80624b46f197b50b0a61b9b1a6
Sirena3112 <sirena3112@rambler.ru>
2022-09-24 11:27:30 +0300

    добавила

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log('Hello world!');
+const ourPlanetName='Земля';
+console.log(ourPlanetName);
+let currentUserName='Maria';
+let surName='Rusyaeva';
+console.log(currentUserName);
+console.log(surName);
+let numberOfArticles= 13;
+console.log(numberOfArticles);
+let isCash= true;
+console.log(isCash);
+let nowTime='12.00';
+console.log(nowTime);
+
+
