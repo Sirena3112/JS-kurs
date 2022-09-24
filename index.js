d74dff84b76efe22285af890fd0ec0a287c37777
Sirena3112 <sirena3112@rambler.ru>
2022-09-24 12:52:13 +0300

    2занятие

--- index.js
+++ index.js
@@ -11,5 +11,23 @@ let isCash= true;
 console.log(isCash);
 let nowTime='12.00';
 console.log(nowTime);
+const surNameourPlenetName = `человек ${currentUserName} живет на планете ${ourPlanetName}`
+console.log(surNameourPlenetName)
+/*const surNameourPlenetName = `человек ${currentUserName} живет на планете ${ourPlanetName}`
+console.log(surNameourPlenetName)*/
+//*красота
+/*let townName;
+console.log(townName);
+let townName = 'NN';
+console.log(townName);*/
+const currentUserNameNowTime = {
+    currentUserName: 'Marya',
+    nowTime: '12.20',
+};
+console.log(currentUserNameNowTime);
+console.log(typeof currentUserName);
+console.log(typeof currentUserNameNowTime);
+console.log(typeof numberOfArticles);
+
 
 
