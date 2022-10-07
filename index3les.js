4d54a9581ae77d51971e4211c11161efd91c26a3
Sirena3112 <sirena3112@rambler.ru>
2022-10-07 21:27:07 +0300

    indexDZ1

---
+++ index3les.js
@@ -0,0 +1,51 @@
+const name = 'Marya';
+const nameToNamber = Number(name);
+//console.log(nameToNamber);
+const age = 26;
+const ageToString = String(age);
+//console.log(tupeof Number(ageToString));
+const nullVaiue = null;
+//console.log(Number(nullVaiue));
+//console.log(Boolean (""));
+//console.log(Boolean(0));
+//console.log(typeof + "45");
+//console.log(typeof Number("45"));
+//console.log(2+2+3);
+/*console.log("2"+2+3);
+console.log("2"+"3");
+console.log(2==='2');
+console.log(2=='2');*/
+console.log(5>4);
+console.log("ананас">"яблоко");
+console.log("2">"12");
+console.log(undefined==null);
+console.log(undefined===null);
+console.log(null=="\n0\n");
+console.log(null===+"\n0\n");
+console.log(""+1+0);
+console.log(""-1+0);
+console.log(true+false);
+console.log(6/"3");
+console.log("2"*"3");
+console.log(4+5+"px");
+console.log("$"+4+5);
+console.log("4"-2);
+console.log("4px"-2);
+console.log(7/0);
+console.log(" -9"+5);
+console.log(" -9"-5);
+console.log(null+1);
+console.log(undefined+1);
+console.log("\t\n"-2);
+const arr = ["матрица","Хоббит","люди х","Властелин колец"];
+console.log (arr);
+console.log (arr[0]);
+arr.push("ОНО");
+const lastIndex = arr.length - 1;
+console.log(lastIndex);
+console.log(arr[lastIndex]);
+
+
+
+
+
